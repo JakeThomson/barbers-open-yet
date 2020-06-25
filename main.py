1a951b20f28a1b1ff639d9ad783c484339932c30
JakeThomson <jakethomson0@gmail.com>
2020-06-25 21:07:58 +0100

    App gets twilio details from env variables and sends text when site is online.

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import bs4 as bs
 import requests
+from twilio.rest import Client
+import os
 
 is_not_open = True
+
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
 
 resp = requests.get("https://ttbbrighton.resurva.com/")
 soup = bs.BeautifulSoup(resp.text, features="html.parser")
@@ -9,4 +15,10 @@ soup = bs.BeautifulSoup(resp.text, features="html.parser")
 status = soup.find_all("h3", class_="title")
 if status[0].contents[0] == '\n\t\t\tBooking is Currently Offline\t\t':
     print("IS OFFLINE")
-
+else:
+    message = client.messages \
+        .create(
+        body='THE BARBERS ARE OPEN!!! - https://ttbbrighton.resurva.com/',
+        from_=os.environ['TWILIO_FROM_NUMBER'],
+        to=os.environ['TWILIO_TO_NUMBER']
+    )
