f1bfb71417536285cf7b45a9912b04c79dfd2288
JakeThomson <jakethomson0@gmail.com>
2022-05-10 17:57:09 +0100

    Changes to poll for Kaytranada tickets

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import json
+import re
+
 import bs4 as bs
 import requests
 from twilio.rest import Client
@@ -8,19 +11,25 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-account_sid = os.environ['TWILIO_ACCOUNT_SID']
-auth_token = os.environ['TWILIO_AUTH_TOKEN']
+account_sid = os.environ['ACCOUNT_SID']
+auth_token = os.environ['AUTH_TOKEN']
 client = Client(account_sid, auth_token)
-poll_interval = (15 * 60)
-from_number = os.environ['TWILIO_FROM_NUMBER']
-to_number = os.environ['TWILIO_TO_NUMBER']
+poll_interval = (2 * 60)
+from_number = os.environ['FROM_NUMBER']
+to_number = os.environ['TO_NUMBER']
 count = 0
+
+last_cheapest_ticket = 106.82
 
 while True:
     count += 1
     try:
-        resp = requests.get("https://ttbbrighton.resurva.com/")
+        resp = requests.get("https://www.viagogo.co.uk/Concert-Tickets/Club-and-dance/Kaytranada-Tickets/E-150085021/")
         soup = bs.BeautifulSoup(resp.text, features="html.parser")
+        pattern = re.compile("eventListings = JSON\.parse\(\"(.*)\"\);")
+        match = pattern.search(str(soup)).group(1)
+        match = match.replace('\\', '')
+        response = json.loads(match)
     except:
         message = client.messages \
             .create(
@@ -32,29 +41,22 @@ while True:
         time.sleep(poll_interval * 2)
         continue
 
-    try:
-        status = soup.find_all("h3", class_="title")
-    except:
+    circle_seating = [item for item in response if "circle" in item['Section'].lower()]
+
+    cheapest = circle_seating[0]['RawPrice']
+
+    if cheapest < last_cheapest_ticket:
         message = client.messages \
             .create(
-                body='THE BARBERS ARE OPEN!!! - https://ttbbrighton.resurva.com/',
+                body=f'Cheaper ticket for sale!! (£{cheapest})',
                 from_=from_number,
                 to=to_number
             )
-        print(f"[{dt.datetime.strftime(dt.datetime.now(), '%d/%m/%Y %H:%M', )}] poll #{count} - ONLINE!!!")
-
-        break
-
-    if status[0].contents[0] == '\n\t\t\tBooking is Currently Offline\t\t':
-        print(f"[{dt.datetime.strftime(dt.datetime.now(), '%d/%m/%Y %H:%M', )}] poll #{count} - OFFLINE")
+        last_cheapest_ticket = cheapest
+        print(f"[{dt.datetime.strftime(dt.datetime.now(), '%d/%m/%Y %H:%M', )}] poll #{count} - CHEAPER TICKETS! (£{cheapest})")
+        time.sleep(poll_interval * 10)
+    else:
+        last_cheapest_ticket = cheapest
+        print(f"[{dt.datetime.strftime(dt.datetime.now(), '%d/%m/%Y %H:%M', )}] poll #{count} - No cheaper tickets found")
         time.sleep(poll_interval)
 
-    else:
-        message = client.messages \
-            .create(
-                body='THE BARBERS ARE OPEN!!! - https://ttbbrighton.resurva.com/',
-                from_=from_number,
-                to=to_number
-            )
-        print(f"[{dt.datetime.strftime(dt.datetime.now(), '%d/%m/%Y %H:%M', )}] poll #{count} - ONLINE!!!")
-        break
